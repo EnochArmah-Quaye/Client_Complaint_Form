df8cc0b1a0b910ee92c30c02b4df89f10f734c95
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2024-05-20 12:00:19 +0000

    Fifth commit
    5

--- adminportal/signup1.php
+++ adminportal/signup1.php
@@ -2,19 +2,7 @@
 
 require_once('../conn.php');
 
-//Connection to a database 
-$servername = "localhost";
-$user= "root";
-$dpassword= "10835954enoch";
-$dbname= "client_complaint_form";
 
-$conn= mysqli_connect($servername,$user,$dpassword,$dbname);
-
-if(!$conn) 
-{
-    die("Connection Failed: ".mysqli_connect_error());
-}
-echo "<p>Connected Successfully</p>";
 
 //Taking inputs from the form
 if(isset($_POST['submit'])){
